2e4db3759030683257ea524d70c8bafaeee16bc8
bmehder <bmehder@gmail.com>
2025-06-20 14:37:37 +0100

    added ramda link

--- src/Main.svelte
+++ src/Main.svelte
@@ -2,6 +2,7 @@
 	<h1>bmehder@TARDIS ~ % <span class="cursor">|</span></h1>
 	<h3>Current Interests:</h3>
 	<ul>
+		<li><a href="https://ramdajs.com/" target="_blank">Ramda</a></li>
 		<li><a href="https://svelte.dev/" target="_blank">Svelte</a></li>
 		<li><a href="https://gleam.run/" target="_blank">Gleam</a></li>
 		<li><a href="https://www.formula1.com/" target="_blank">Formula 1</a></li>
