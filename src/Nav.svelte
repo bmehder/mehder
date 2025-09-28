ad9d877decb96ed215ac0455660f437447302c5c
bmehder <bmehder@gmail.com>
2025-09-28 16:56:33 +0100

    added new link for npm

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -8,7 +8,10 @@
 		<li on:click={aboutClickHandler}>/about</li>
 		<li on:click={travelClickHandler}>/travel</li>
 		<li>
-			<a href="https://github.com/bmehder" target="_blank">/code</a>
+			<a href="https://github.com/bmehder" target="_blank">/github</a>
+		</li>
+		<li>
+			<a href="https://github.com/bmehder" target="_blank">/npm</a>
 		</li>
 	</ul>
 </nav>
